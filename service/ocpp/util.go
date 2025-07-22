ab56d65ddd4bae861d4658d1e03491ac097e5cee
Squishmeist <ayelee02@gmail.com>
2025-07-22 11:56:00 +0100

    docs: added to util functions

--- service/ocpp/util.go
+++ service/ocpp/util.go
@@ -6,11 +6,13 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 )
 
+// asType performs a generic type assertion for interface{} to *T.
 func asType[T any](data interface{}) (*T, bool) {
     val, ok := data.(*T)
     return val, ok
 }
 
+// unmarshalAndValidate unmarshals a JSON payload into a struct of type T and validates it.
 func unmarshalAndValidate[T any](payload []byte) (*T, error) {
     validate := validator.New()
     var obj T
